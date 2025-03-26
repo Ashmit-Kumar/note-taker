73ddd00a381d3e4f478db6070d988b7a09204cd1
Ashmit Kumar <ashmitkumar1020@gmail.com>
2025-03-26 11:51:48 +0530

    Revert "Update docker-compose.yml"
    This reverts commit 4ebe556cf6d76b0fd3de89eaab7137d727c3cd25.

--- src/App.jsx
+++ src/App.jsx
@@ -47,27 +47,6 @@ function App() {
     <div className="App">
       <h1>Note Taker</h1>
       {/* Your JSX code */}
-	<form onSubmit={addNote} >
-	  <input
-		type="text" placeholder="Title" value={newNote.title} 
-		onChange={(e) => setNewNote({...newNote,title:e.target.value})} />
-	  <textarea
-		type= "text" 
-		placeholder="Content"
-		value={newNote.content} 
-		onChange= {(e) => setNewNote({...newNote,content:e.target.value})}></textarea>
-		<button type="submit"> Add Note </button>
-	</form>
-	{/* list of notes */}
-	<ul>
-		{notes.map((note) =>(
-		<li key={note.id}>
-		<h3> {note.title} </h3>
-		<p> {note.content} </p>
-		<button onClick={()=>deleteNode(note.id)} > Delete </button>
-		</li>
-	        ))}
-	</ul>
     </div>
   );
 }
